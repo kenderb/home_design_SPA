238db6168c6b95cb24779126835011c37b3f19dd
kenderb <40863532+kenderb@users.noreply.github.com>
2021-03-19 14:11:53 -0500

    Create the production build

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     publicPath: '',
   },
-  mode: 'development',
+  mode: 'production',
   devServer: {
     contentBase: './dist',
     host: 'localhost',
